92840629860c2ba4f992bccd9a7aaf0d579061ba
Mantas Brasiunas <hex0@live.com>
2025-08-17 14:13:34 +0100

    chore: auto commit after successful build

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,7 +6,6 @@ const config: StorybookConfig = {
     '@storybook/addon-webpack5-compiler-swc',
     '@storybook/addon-docs',
     '@storybook/addon-onboarding',
-    '@storybook/addon-test',
   ],
   framework: {
     name: '@storybook/react-webpack5',
